cef914a57d92de0ca8ac73cd195aef180f3fd107
Chance Vodnoy <esgameco@gmail.com>
2021-07-10 18:36:03 -0700

    Changed resources to dungeon crawl

--- client/js/client.js
+++ client/js/client.js
@@ -1,4 +1,5 @@
 import Actions from './helper/actions.js';
+import ResourceManager from './helper/resources.js';
 
 const socket = new WebSocket('ws://localhost:8080/');
 
@@ -14,5 +15,15 @@ socket.addEventListener('open', (ev) => {
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'Blue';
-ctx.fillRect(100, 100, 10, 10);
+const resourceManager = new ResourceManager();
+
+// ctx.fillStyle = 'Blue';
+// ctx.fillRect(100, 100, 10, 10);
+
+const images = ['item/potion/black', 'item/potion/black', 'item/potion/black', 'item/potion/black'].map((n) => resourceManager.getResource(n));
+
+images.forEach((img, i) => {
+    img.addEventListener('load', () => { 
+        ctx.drawImage(img, i*50, 0);
+    });
+});
--- client/js/helper/resources.js
+++ client/js/helper/resources.js
@@ -1,3 +1,19 @@
-export default {
+class ResourceManager {
+    constructor() {
+        this.resources = {};
+    }
 
-};
+    // Returns resource if it exists, otherwise downloads resource
+    getResource(rsc) {
+        if (this.resources[rsc])
+            return this.resources[rsc];
+        else {
+            let img = new Image();
+            img.src = `/images/${rsc}.png`;
+            this.resources[rsc] = img;
+            return img;
+        }
+    }
+}
+
+export default ResourceManager;
